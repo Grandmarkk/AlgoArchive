ddcd28a78975a6c2df810704c011b74286d638ba
Mark <marksun772@foxmail.com>
2024-06-07 15:57:44 +0800

    BST implemented

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,14 @@
 import sort
-from search import z_algorithm
+from tree.binary_search_tree import BST, BST_Node
 
 if __name__ == "__main__":
-    print(z_algorithm.z_algo("abababacb"))
+    root = BST_Node(3)
+    bst = BST(root)
+    bst.insert(2)
+    bst.insert(1)
+    bst.insert(5)
+    bst.insert(4)
+    bst.insert(6)
+    bst.insert(6)
+    bst.delete(3)
+    print(bst.traversal())
---
+++ src/tree/binary_search_tree.py
@@ -0,0 +1,84 @@
+class BST_Node:
+    def __init__(self, key) -> None:
+        self.key = key
+        self.left = None
+        self.right = None
+
+class BST:
+    def __init__(self, root) -> None:
+        self.root = root
+
+    def search(self, target) -> BST_Node:
+        def helper(node, target):
+            if node == None:
+                return None
+            if node.key == target:
+                return node
+            if node.key > target:
+                return helper(node.left, target)
+            elif node.key < target:
+                return helper(node.right, target)
+        return helper(self.root, target)
+
+    def insert(self, key) -> None:
+        def helper(node, key):
+            if node.key == key:
+                return
+            if node.key > key:
+                if node.left == None:
+                    node.left = BST_Node(key)
+                    return
+                else:
+                    helper(node.left, key)
+            elif node.key < key:
+                if node.right == None:
+                    node.right = BST_Node(key)
+                    return
+                else:
+                    helper(node.right, key)
+        helper(self.root, key)
+
+    def delete(self, target) -> None:
+        def minValueNode(node):
+            current = node
+            # Loop down to find the leftmost leaf
+            while current and current.left is not None:
+                current = current.left
+            return current
+        
+        def helper(node, target):
+            if node is None:
+                return node
+            if target < node.key:
+                node.left = helper(node.left, target)
+            elif target > node.key:
+                node.right = helper(node.right, target)
+            else:
+                # Node with only one child or no child
+                if node.left is None:
+                    temp = node.right
+                    node = None
+                    return temp
+                elif node.right is None:
+                    temp = node.left
+                    node = None
+                    return temp
+                # Node with two children:
+                temp = minValueNode(node.right)
+                node.key = temp.key
+                node.right = helper(node.right, temp.key)
+            return node
+        self.root = helper(self.root, target)
+                
+
+    def traversal(self):
+        def helper(node):
+            if node == None:
+                return
+            helper(node.left)
+            keys.append(node.key)
+            helper(node.right)
+
+        keys = []
+        helper(self.root)
+        return keys
