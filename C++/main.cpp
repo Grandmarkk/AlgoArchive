3a2874fadd5d513c7160adc6c59443ed34408838
Mark Sun <marksun772@foxmail.com>
2025-05-17 17:40:47 +0800

    Bubble sort & selection sort implemented in c++.

--- C++/main.cpp
+++ C++/main.cpp
@@ -1,5 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include <iostream>
 #include "sort.cpp"
+
+using namespace std;
+
+int main()
+{
+    vector<int> testArr = {1, 0, 4, 2, 5, 3, 7, 9, 6};
+    vector<int> sorted = selectionSort(testArr);
+    for (int i : sorted)
+    {
+        cout << i << " ";
+    }
+    return 0;
+}
--- C++/sort.cpp
+++ C++/sort.cpp
@@ -2,3 +2,50 @@
 #include <iostream>
 
 using namespace std;
+
+vector<int> bubbleSort(vector<int> arr)
+{
+    int length = arr.size();
+    for (int i = 0; i < length; i++)
+    {
+        bool swaped = false;
+        for (int j = 0; j < length - i - 1; j++)
+        {
+            // Swap
+            if (arr[j + 1] < arr[j])
+            {
+                swaped = true;
+                int temp = arr[j + 1];
+                arr[j + 1] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        if (!swaped)
+        {
+            return arr;
+        }
+    }
+    return arr;
+}
+
+vector<int> selectionSort(vector<int> arr)
+{
+    int left = 0;
+    int right = arr.size() - 1;
+    while (left < right)
+    {
+        int minIdx = left;
+        // Find min in unsorted part
+        for (int i = left + 1; i <= right; i++)
+        {
+            if (arr[i] < arr[minIdx])
+            {
+                minIdx = i;
+            }
+        }
+        // Swap
+        swap(arr[minIdx], arr[left]);
+        left++;
+    }
+    return arr;
+}
