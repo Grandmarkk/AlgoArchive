27d5719848571d06ff2bb43e9fbc70e69b983455
Mark Sun <marksun772@foxmail.com>
2025-05-22 22:20:00 +0800

    Trie implemented in c++.

--- C++/trie.cpp
+++ C++/trie.cpp
@@ -19,11 +19,57 @@ public:
         root = new TrieNode();
     }
 
+    /**
+     * Insert a word into the trie
+     */
     void insert(string word)
     {
+        TrieNode *cur = root;
+        for (char c : word)
+        {
+            // Character not found
+            if (cur->children.find(c) == cur->children.end())
+            {
+                cur->children[c] = new TrieNode();
+            }
+            cur = cur->children[c];
+        }
+        cur->isLeaf = true;
     }
 
-    void search(string word)
+    /**
+     * Search if the word is in the trie
+     */
+    bool search(string word)
     {
+        TrieNode *cur = root;
+        for (char c : word)
+        {
+            // Word not in trie
+            if (cur->children.find(c) == cur->children.end())
+            {
+                return false;
+            }
+            cur = cur->children[c];
+        }
+        return cur->isLeaf;
+    }
+
+    /**
+     * Search if the given string is a prefix in the trie
+     */
+    bool isPrefix(string prefix)
+    {
+        TrieNode *cur = root;
+        for (char c : prefix)
+        {
+            // Prefix not in trie
+            if (cur->children.find(c) == cur->children.end())
+            {
+                return false;
+            }
+            cur = cur->children[c];
+        }
+        return true;
     }
 };
